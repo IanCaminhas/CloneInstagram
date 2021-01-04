28af381557e04578adc2735fb6f9fbc641a3431d
IanCaminhas <iancaminhas25@hotmail.com>
2021-01-04 19:05:51 -0300

    componente comentarios

--- App.js
+++ App.js
@@ -6,10 +6,20 @@ import Post from './src/components/Post'
 
 export default class App extends Component{
   render(){
+
+    const comments =[{
+      nickname: 'João Elena Silva',
+      comment: 'Excelente foto!'
+    },{
+      nickname: 'Rian Pereira',
+      comment: 'Muito ruim! faço melhor hahahah'
+    }]
+
     return (
       <View style={{flex:1}}>
         <Header/>
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')} 
+        comments ={comments}/>
       </View>
   
       
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,7 @@
 import React, {Component} from  'react'
 import {StyleSheet, View, Image, Dimensions} from  'react-native'
 import Author from './Author'
-
+import Comments from  './Comments'
 
 class Post extends Component {
     render(){
@@ -9,6 +9,7 @@ class Post extends Component {
             <View style={StyleSheet.container}>
                 <Image source={this.props.image} style={styles.image}/>
                 <Author email='fulano@gmail.com' nickname='Fulano de tal' />
+                <Comments comments={this.props.comments} />
             </View>
 
         )
